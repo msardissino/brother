ace71db60c0422bf1d9d6dc1adebca7c69537c47
msardissino <msardissino@yahoo.com.ar>
2018-12-07 01:13:40 -0300

    first commit

---
+++ js/borthers-scripts.js
@@ -0,0 +1,7 @@
+const $btnHamburger = document.getElementById('hamburger');
+const $spanburger = document.getElementById('spanburger');
+
+$btnHamburger.addEventListener('click', ()=>{
+  $btnHamburger.classList.toggle('is-active');
+  
+});
