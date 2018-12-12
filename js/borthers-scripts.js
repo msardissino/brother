6e73e9d79d1edc78fc8c50da16c9c2dca18416a2
msardissino <msardissino@yahoo.com.ar>
2018-12-12 19:06:24 -0300

    4to commit

--- js/borthers-scripts.js
+++ js/borthers-scripts.js
@@ -1,3 +1,4 @@
+// Burguer btn Movil menu
 const $btnHamburger = document.getElementById('hamburger');
 const $spanburger = document.getElementById('spanburger');
 
@@ -5,8 +6,13 @@ $btnHamburger.addEventListener('click', ()=>{
   $btnHamburger.classList.toggle('is-active');
   
 });
-
+// Fin Burguer btn Movil menu
+// Light Box
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+// Fin Light Box
+
+// Preguntas Frecuentes
+//Fin Preguntas Frecuentes
