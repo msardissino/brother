154ec578ff7707532cd55f7dcdd8fa0914ed8253
msardissino <msardissino@yahoo.com.ar>
2018-12-13 00:27:40 -0300

    5to commit

--- js/borthers-scripts.js
+++ js/borthers-scripts.js
@@ -15,4 +15,27 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 // Fin Light Box
 
 // Preguntas Frecuentes
+const $preg1 = document.querySelector('[data-target="#collapseOne"]');
+const $span1 = document.querySelector('[data-target="spanOne"]');
+const $preg2 = document.querySelector('[data-target="#collapseTwo"]');
+const $span2 = document.querySelector('[data-target="spanTwo"]');
+const $preg3 = document.querySelector('[data-target="#collapseThree"]');
+const $span3 = document.querySelector('[data-target="spanThree"]');
+
+$preg1.addEventListener('click', ()=>{
+  $span1.classList.toggle('icon-menos');
+  $span1.classList.toggle('icon-mas');
+
+});
+$preg2.addEventListener('click', ()=>{
+  $span2.classList.toggle('icon-menos');
+  $span2.classList.toggle('icon-mas');
+
+});
+$preg3.addEventListener('click', ()=>{
+  $span3.classList.toggle('icon-menos');
+  $span3.classList.toggle('icon-mas');
+
+});
+
 //Fin Preguntas Frecuentes
